b09cac59a11c610585233f399bc871c45ecb3e23
Jovan <jovansaputralaksana_2226250050@mhs.mdp.ac.id>
2023-10-20 18:46:53 +0700

    lanjutan-wisata-candi

--- wisata_candi/lib/main.dart
+++ wisata_candi/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:wisata_candi/screens/detail_screen.dart';
+import 'package:wisata_candi/data/candi_data.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,12 +12,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Wisata Candi",
-      theme: ThemeData(primarySwatch: Colors.brown),
-      home: const WidgetCandi(),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: DetailScreen(candi: candiList[0]),
     );
   }
-}
-
-class WidgetCandi extends StatelessWidget{
-  
 }
