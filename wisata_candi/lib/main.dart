62304d7a1a4946b841add25654a852b92ffcd44b
Jovan <jovansaputralaksana_2226250050@mhs.mdp.ac.id>
2023-10-27 18:33:37 +0700

    pertemuan 14

--- wisata_candi/lib/main.dart
+++ wisata_candi/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wisata_candi/screens/SignIn_screen.dart';
 import 'package:wisata_candi/screens/detail_screen.dart';
 import 'package:wisata_candi/data/candi_data.dart';
 import 'package:wisata_candi/screens/profile_screen.dart';
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: ProfileScreen(),
+      home: SignInScreen(),
     );
   }
 }
