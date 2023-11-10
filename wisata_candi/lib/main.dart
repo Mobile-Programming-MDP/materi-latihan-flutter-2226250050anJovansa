29907ce3073e7701e7053e9f96f91312f3ad21cf
Jovan <jovansaputralaksana_2226250050@mhs.mdp.ac.id>
2023-11-10 18:06:15 +0700

    home screen

--- wisata_candi/lib/main.dart
+++ wisata_candi/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:wisata_candi/screens/detail_screen.dart';
 import 'package:wisata_candi/data/candi_data.dart';
 import 'package:wisata_candi/screens/profile_screen.dart';
 import 'package:wisata_candi/screens/search_screen.dart';
+import 'package:wisata_candi/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: SearchScreen(),
+      home: Homescreen(),
     );
   }
 }
